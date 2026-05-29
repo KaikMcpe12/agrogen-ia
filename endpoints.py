62cf8eec70dfdf3c5c5e0650aa1c8fda8c41b26d
Lucas <lucasrdsprof@gmail.com>
2026-05-28 22:41:43 -0300

    subindo projeto

--- endpoints.py
+++ endpoints.py
@@ -1,16 +1,42 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from sqlalchemy.orm import Session
 
-#endpoint
 @app.post("/auth/register")
-def register_user(user: UserRegister):
+def register_user(user: UserRegister, db: Session = Depends(get_db)):
 
+    # valida tipo
     if user.tipo_usuario not in ["admin", "comum"]:
         raise HTTPException(
             status_code=400,
             detail="tipo_usuario deve ser 'admin' ou 'comum'"
         )
 
+    # verifica email
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(status_code=400, detail="Email já existe")
+
+    # verifica cpf
+    if db.query(User).filter(User.cpf == user.cpf).first():
+        raise HTTPException(status_code=400, detail="CPF já existe")
+
+    # cria usuário
+    new_user = User(
+        nome=user.nome,
+        cpf=user.cpf,
+        email=user.email,
+        tipo_usuario=user.tipo_usuario
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
     return {
         "mensagem": "Usuário cadastrado com sucesso",
-        "usuario": user
+        "usuario": {
+            "id": new_user.id,
+            "nome": new_user.nome,
+            "email": new_user.email,
+            "tipo_usuario": new_user.tipo_usuario
+        }
     }
