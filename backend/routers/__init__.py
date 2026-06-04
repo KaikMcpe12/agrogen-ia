db3f4cd40cca83d24a3fc8a4878c4af8a773372b
Pedro <joaopedropereira2209@gmail.com>
2026-06-04 14:45:02 -0300

    feat: added export on router

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,3 +1,4 @@
 from routers.animal_router import router as animal_router
 from routers.user_router import router as user_router
 from routers.diagnostico_router import router as diagnostico_router
+from routers.fazenda_router import router as fazenda_router
