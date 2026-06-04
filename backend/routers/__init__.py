e62e214ff6ab47c73f2054d51433ac02423b6e8b
Pedro <joaopedropereira2209@gmail.com>
2026-06-04 15:54:42 -0300

    feat: added export analise_ia on router

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -2,3 +2,4 @@ from routers.animal_router import router as animal_router
 from routers.user_router import router as user_router
 from routers.diagnostico_router import router as diagnostico_router
 from routers.fazenda_router import router as fazenda_router
+from routers.analise_ia_router import router as analise_ia_router
