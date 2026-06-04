ae8829c4a17a9650148949481a00b349e28d4500
Pedro <joaopedropereira2209@gmail.com>
2026-06-04 14:42:43 -0300

    refactor: refactored fazenda_model

--- backend/models/fazenda_model.py
+++ backend/models/fazenda_model.py
@@ -9,12 +9,6 @@ from sqlalchemy.sql import func
 from models.base import Base
 from models.enums import TipoProducao
 
-ESTADOS_VALIDOS = (
-    'AC','AL','AP','AM','BA','CE','DF','ES','GO',
-    'MA','MT','MS','MG','PA','PB','PR','PE','PI',
-    'RJ','RN','RS','RO','RR','SC','SP','SE','TO'
-)
-
 class FazendaModel(Base):
     __tablename__ = "fazendas"
 
