191d624e510e3c5a512a2c90120adc4324ebeb2c
Pedro <joaopedropereira2209@gmail.com>
2026-06-04 14:30:12 -0300

    feat: created class declarative base

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -6,10 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.sql import func
 
 from models.enums import Perfil
-
-class Base(DeclarativeBase):
-    pass
-
+from models.base import Base
 
 class User(Base):
     __tablename__ = "usuarios"
